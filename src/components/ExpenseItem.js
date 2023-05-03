2ffabe24b5866a65f188978987cc0b41125848e9
Amit Sali <79369803+amit-sali@users.noreply.github.com>
2023-05-03 06:10:36 +0000

    the error is sorted out and page is running  <div>{ props.date.toISOString() }</div> ere we entered wrong amount parameter where we have to put date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,7 +7,7 @@
   
     return (
         <div className="expense-item" >
-            <div>{ props.amount.toISOString() }</div>
+            <div>{ props.date.toISOString() }</div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
